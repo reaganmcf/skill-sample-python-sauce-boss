ee05c1c3dc82284d821d53b86c34168935e6fa0b
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2019-10-14 11:53:02 -0400

    [ FIX ] added .venv to repo

---
+++ .venv/skill_env/lib/python3.7/site-packages/ask_sdk_model/interfaces/alexa/presentation/apl/__init__.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+#
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+#
+from __future__ import absolute_import
+
+from .position import Position
+from .idle_command import IdleCommand
+from .open_url_command import OpenUrlCommand
+from .user_event import UserEvent
+from .play_media_command import PlayMediaCommand
+from .render_document_directive import RenderDocumentDirective
+from .audio_track import AudioTrack
+from .parallel_command import ParallelCommand
+from .animated_opacity_property import AnimatedOpacityProperty
+from .set_value_command import SetValueCommand
+from .scroll_command import ScrollCommand
+from .auto_page_command import AutoPageCommand
+from .speak_item_command import SpeakItemCommand
+from .video_source import VideoSource
+from .animate_item_repeat_mode import AnimateItemRepeatMode
+from .animated_property import AnimatedProperty
+from .skew_transform_property import SkewTransformProperty
+from .animated_transform_property import AnimatedTransformProperty
+from .animate_item_command import AnimateItemCommand
+from .transform_property import TransformProperty
+from .align import Align
+from .highlight_mode import HighlightMode
+from .sequential_command import SequentialCommand
+from .runtime import Runtime
+from .execute_commands_directive import ExecuteCommandsDirective
+from .move_transform_property import MoveTransformProperty
+from .clear_focus_command import ClearFocusCommand
+from .command import Command
+from .set_focus_command import SetFocusCommand
+from .control_media_command import ControlMediaCommand
+from .set_page_command import SetPageCommand
+from .scale_transform_property import ScaleTransformProperty
+from .send_event_command import SendEventCommand
+from .alexa_presentation_apl_interface import AlexaPresentationAplInterface
+from .set_state_command import SetStateCommand
+from .rotate_transform_property import RotateTransformProperty
+from .scroll_to_index_command import ScrollToIndexCommand
+from .speak_list_command import SpeakListCommand
+from .media_command_type import MediaCommandType
+from .component_state import ComponentState
